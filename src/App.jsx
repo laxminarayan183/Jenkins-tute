c3272ad0edf2a0efe4406b6f7b22b3f43462eb63
laxminarayan <laxminarayankonda7@gmail.com>
2025-02-11 21:16:17 +0530

    jenkins file added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4TiDyb_Fudb03_WhC3MMOqX0HoYbjK2s05U5lTlPeAbVKIjOcgCu-gvZCBYdfyh2JWf4&usqp=CAU"
+            className="App-logo"
+            alt="logo"
+          />
+          <a
+            className="App-link"
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Jenkins on Udemy
+          </a>
+        </header>
+        <p>Application version: 1</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
